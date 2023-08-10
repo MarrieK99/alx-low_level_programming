7c4f6c5bd828e06f712383a0a2ded330dbd25cd8
MarrieK99 <maryannekrnj@gmail.com>
2023-08-10 10:32:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#!/bin/bash
+
 #include <stdio.h>
 
 int main(void)
 {
-    return puts("Programming is like building a multilingual puzzle"), 0;
+return puts("Programming is like building a multilingual puzzle"), 0;
 }
