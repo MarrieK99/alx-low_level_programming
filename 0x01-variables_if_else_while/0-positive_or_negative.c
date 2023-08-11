fc59e5b3f4f73fd11d536426d5b717c1b8abbd56
MarrieK99 <maryannekrnj@gmail.com>
2023-08-11 06:08:23 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,7 @@ int main(void)
 	srand(time(0));
 
 	/* Generate a random number between -100 and 100 */
-	n = rand() % 201 - 100;
+	n = rand() % n = rand() % (RAND_MAX + 1) - RAND_MAX / 2;
 
 	/* Print the generated number */
 	printf("The number is: %d\n", n);
