0e161b4388a431eab3385037774ecc6da9c463fc
MarrieK99 <maryannekrnj@gmail.com>
2023-08-11 05:42:53 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,11 +10,16 @@
  */
 int main(void)
 {
-char digit;
-
-for (digit = '0'; digit <= '9'; digit++)
-putchar(digit);
-
+putchar('0' + 0);
+putchar('0' + 1);
+putchar('0' + 2);
+putchar('0' + 3);
+putchar('0' + 4);
+putchar('0' + 5);
+putchar('0' + 6);
+putchar('0' + 7);
+putchar('0' + 8);
+putchar('0' + 9);
 putchar('\n');
 
 return (0);
