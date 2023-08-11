6c850f64cfe4c8aafc33620cf9de65204cc84034
MarrieK99 <maryannekrnj@gmail.com>
2023-08-11 05:36:07 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,9 +14,9 @@ int number1;
 int number2;
 
 /* Print the combinations of two two-digit numbers using putchar */
-for (number1 = 0; number1 <= 99; number1++)
+for (number1 = 0; number1 <= 98; number1++)
 {
-for (number2 = number1; number2 <= 99; number2++)
+for (number2 = number1 + 1; number2 <= 99; number2++)
 {
 putchar(number1 / 10 + '0');
 putchar(number1 % 10 + '0');
@@ -24,7 +24,7 @@ putchar(' ');
 putchar(number2 / 10 + '0');
 putchar(number2 % 10 + '0');
 
-if (number1 < 99 || number2 < 99)
+if (number1 < 98 || number2 < 99)
 {
 putchar(',');
 putchar(' ');
