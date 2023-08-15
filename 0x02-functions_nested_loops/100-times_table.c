f5a9f0574f5e6e20df5bcadb88d9086fc0ff54bd
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 16:51:14 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,47 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_times_table - Prints the n times table
+ * print_times_table - Prints the n times table, starting with 0.
  * @n: The value specifying the times table
  */
 void print_times_table(int n)
 {
-if (n < 0 || n > 15)
-return;
+    if (n < 0 || n > 15)
+        return;
 
-int i;
-int j;
+    int i, j;
 
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-int result = i * j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            int result = i * j;
 
-if (j == 0)
-printf("%d", result);
-else
-printf(", %3d", result);
-}
-printf("\n");
-}
+            if (j == 0)
+                _putchar(result + '0');
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (result < 10)
+                    _putchar(' ');
+                if (result < 100)
+                    _putchar(' ');
+
+                _putchar((result / 100) + '0');
+                _putchar(((result / 10) % 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
 
 int main(void)
 {
-int n = 10; /* Change this to the desired n value */
-print_times_table(n);
+    int n = 10; /* Change this to the desired n value */
+    print_times_table(n);
 
 return (0);
 }
