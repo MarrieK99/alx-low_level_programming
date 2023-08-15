b2bbb102427a85acaf15b1423511576ddf49b61b
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 16:09:48 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,21 +10,21 @@
  */
 int main(void)
 {
-    int i;
-    long int fib1 = 1, fib2 = 2, next_fib;
+int i;
+long int fib1 = 1, fib2 = 2, next_fib;
 
-    printf("%ld, %ld", fib1, fib2);
+printf("%ld, %ld", fib1, fib2);
 
-    for (i = 2; i < 50; i++)
-    {
-        next_fib = fib1 + fib2;
-        printf(", %ld", next_fib);
+for (i = 2; i < 50; i++)
+{
+next_fib = fib1 + fib2;
+printf(", %ld", next_fib);
 
-        fib1 = fib2;
-        fib2 = next_fib;
-    }
-
-    printf("\n");
-
-    return (0);
+fib1 = fib2;
+fib2 = next_fib;
 }
+
+printf("\n");
+return (0);
+}
+
