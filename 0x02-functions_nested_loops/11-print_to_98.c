b980e9ceb5371f94ca482a329516782e39638095
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 14:19:30 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+void print_number(int n);
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
@@ -6,20 +6,53 @@
  */
 void print_to_98(int n)
 {
-while (n != 98)
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar(',');
-_putchar(' ');
-
-if (n < 98)
-n++;
+if (n <= 98) {
+for (; n < 98; n++) {
+print_number(n);
+print_char(',');
+print_char(' ');
+}
+}
 else
-n--;
+{
+for (; n > 98; n--) {
+print_number(n);
+print_char(',');
+print_char(' ');
+}
+}
+print_number(98);
+print_char('\n');
 }
 
-_putchar('9');
-_putchar('8');
-_putchar('\n');
+/* Custom print functions */
+
+/* Print a character to standard output */
+void print_char(char c);
+
+/* Print an integer to standard output */
+void print_number(int n)
+{
+int divisor = 1;
+
+if (n < 0) {
+print_char('-');
+n = -n;
+}
+
+while (n / divisor > 9) {
+divisor *= 10;
+}
+
+while (divisor != 0) {
+print_char(n / divisor + '0');
+n %= divisor;
+divisor /= 10;
+}
+}
+
+/* Custom print_char implementation */
+void print_char(char c)
+{
+    /* Implement your custom printing mechanism here */
 }
