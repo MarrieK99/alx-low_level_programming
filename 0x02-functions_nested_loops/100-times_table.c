a800ae1cc3153e2b12d1c874e97901ff0ce988de
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 16:56:19 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,42 +6,43 @@
  */
 void print_times_table(int n)
 {
-    if (n < 0 || n > 15)
-        return;
+if (n < 0 || n > 15)
+return;
 
-    int i, j;
+int i;
+int j;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int result = i * j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int result = i * j;
 
-            if (j == 0)
-                _putchar(result + '0');
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+if (j == 0)
+_putchar(result + '0');
+else
+{
+_putchar(',');
+_putchar(' ');
 
-                if (result < 10)
-                    _putchar(' ');
-                if (result < 100)
-                    _putchar(' ');
+if (result < 10)
+_putchar(' ');
+if (result < 100)
+_putchar(' ');
 
-                _putchar((result / 100) + '0');
-                _putchar(((result / 10) % 10) + '0');
-                _putchar((result % 10) + '0');
+_putchar((result / 100) + '0');
+_putchar(((result / 10) % 10) + '0');
+_putchar((result % 10) + '0');
             }
-        }
-        _putchar('\n');
-    }
+}
+_putchar('\n');
+}
 }
 
 int main(void)
 {
-    int n = 10; /* Change this to the desired n value */
-    print_times_table(n);
+int n = 10; /* Change this to the desired n value */
+print_times_table(n);
 
 return (0);
 }
