06821cd0dbfb3f950fb6eedfa9d0f4b1b466c44f
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 14:12:58 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,38 +6,20 @@
  */
 void print_to_98(int n)
 {
-if (n <= 98)
-{
-while (n <= 98)
+while (n != 98)
 {
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
-
-if (n != 98)
-{
 _putchar(',');
 _putchar(' ');
-}
 
+if (n < 98)
 n++;
-}
-}
 else
-{
-while (n >= 98)
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-
 n--;
 }
-}
 
+_putchar('9');
+_putchar('8');
 _putchar('\n');
 }
