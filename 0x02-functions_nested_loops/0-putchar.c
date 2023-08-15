4e6cfbde20fbc1a738a6ae8d55cf4f1949719047
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 10:50:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,19 +4,19 @@
  * print_putchar - Prints _putchar, followed by a new line
  *
  * Description:
- * This function prints the string "_putchar", followed by a new line character,
+ * function prints string "_putchar", followed by a new line character,
  * using the write system call to the standard output.
  */
 void print_putchar(void)
 {
-    write(1, "_putchar\n", 9);
+write(1, "_putchar\n", 9);
 }
 
 /**
  * main - Entry point of the program
  *
  * Description:
- * This is the main function that serves as the entry point of the program.
+ * main function serves as the entry point of the program.
  * It calls the print_putchar function to print "_putchar", followed by a new line,
  * and returns 0 to indicate successful execution.
  *
@@ -24,6 +24,6 @@ void print_putchar(void)
  */
 int main(void)
 {
-    print_putchar();
-    return (0);
+print_putchar();
+return (0);
 }
