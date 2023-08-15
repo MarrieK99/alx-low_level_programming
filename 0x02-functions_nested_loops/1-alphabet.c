bc20217e546877fc6574940b1b76da58536ee847
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 11:31:06 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,20 @@
-#include <stdio.h>
-
-int _putchar(int c);
+#include "main.h"
 
 /**
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
  *
- * Uses _putchar twice
- *
- * Prints the lowercase alphabet, one character per line
+ * Description:
+ * This function prints the lowercase alphabet in a continuous sequence,
+ * from 'a' to 'z', followed by a newline character.
  */
-void print_alphabet(void) {
-char ch = 'a'; /* declare and initialize current letter */
+void print_alphabet(void)
+{
+char letter = 'a'; /* Start with the letter 'a'*/
 
-while (ch <= 'z') { /* iterate over the lowercase alphabet */
-_putchar(ch++); /* print and increment current letter */
+while (letter <= 'z') /* Loop through the alphabet*/
+{
+_putchar(letter); /* Print the current letter*/
+letter++; /*Move to the next letter*/
 }
-
-_putchar('\n'); /* print a newline character */
-}
-
-int main(void) {
-print_alphabet();
-return (0);
+_putchar('\n'); /* Print a newline after the alphabet*/
 }
