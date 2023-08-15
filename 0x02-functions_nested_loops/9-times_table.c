e9c6e9125c8775eb96bcdf9d88fb09a9d8e545cd
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 13:54:00 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,26 +5,28 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        _putchar('0'); /* Print the first cell */
+for (row = 0; row <= 9; row++)
+{
+_putchar('0'); /* Print the first cell */
 
-        for (column = 1; column <= 9; column++)
-        {
-            _putchar(','); /* Print the separator */
+for (column = 1; column <= 9; column++)
+{
+_putchar(','); /* Print the separator */
 
-            product = row * column;
+product = row * column;
 
-            /* Print a space before single-digit numbers */
-            if (product <= 9)
-                _putchar(' ');
+/* Print a space before single-digit numbers */
+if (product <= 9)
+_putchar(' ');
+/* Print tens digit of product */
+if (product >= 10)
+_putchar((product / 10) + '0');
 
-            _putchar(' '); /* Print a space */
-            _putchar(product + '0'); /* Print the product */
-        }
+_putchar((product % 10) + '0'); /* Print ones digit of product */
+}
 
-        _putchar('\n'); /* Move to the next row */
-    }
+_putchar('\n'); /* Move to the next row */
+}
 }
