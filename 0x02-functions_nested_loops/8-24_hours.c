5b47daca85d870ce37eddb9bcb5a5f12e098520c
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 13:41:22 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-_putchar((hour / 10) + '0'); // Print tens digit of hour
-_putchar((hour % 10) + '0'); // Print ones digit of hour
+_putchar((hour / 10) + '0'); /* Print tens digit of hour*/
+_putchar((hour % 10) + '0'); /* Print ones digit of hour*/
 _putchar(':');
-_putchar((minute / 10) + '0'); // Print tens digit of minute
-_putchar((minute % 10) + '0'); // Print ones digit of minute
+_putchar((minute / 10) + '0'); /* Print tens digit of minute*/
+_putchar((minute % 10) + '0'); /* Print ones digit of minute*/
 _putchar('\n');
 }
 }
