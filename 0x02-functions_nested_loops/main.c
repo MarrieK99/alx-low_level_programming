3473fc4208fffcd321760cb67dfa35fdb121a332
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 11:35:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This function calls print_alphabet to print the lowercase alphabet,
+ * followed by a new line, and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
