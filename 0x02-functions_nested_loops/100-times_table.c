521ac330a028b80e42d21d41bbe8feb17710862a
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 15:24:45 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,18 +6,31 @@
  */
 void print_times_table(int n)
 {
-int i, j;
+int i, j, product;
 char digit;
 
 if (n > 15 || n < 0)
 return;
 
-for (i = 0; i <= 10; i++)
+for (i = 0; i <= n; i++)
 {
+_putchar('0'); /* Print the first digit */
+_putchar(','); /* Print comma */
+_putchar(' '); /* Print space */
+
 for (j = 1; j <= n; j++)
 {
-int product = i * j;
-
+product = i * j;
+/* Print leading spaces for single-digit numbers */
+if (product < 10 && j != 1)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else if (product < 100 && j != 1)
+{
+_putchar(' ');
+}
 if (product >= 100)
 {
 digit = (product / 100) + '0';
@@ -38,10 +51,7 @@ if (j < n)
 {
 _putchar(',');
 _putchar(' ');
-if (i * (j + 1) < 10)
-_putchar(' ');
 }
 }
 _putchar('\n');
-}
 }
