e4782fd8e2ffd9fb9b673e91d329859503ac822e
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 10:38:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,12 @@
  *
  * Return: void
  */
-void print_putchar(void)//print_putchar - prints _putchar, followed by a new line//
+void print_putchar(void) /* print_putchar - prints _putchar, followed by a new line*/
 {
 write(1, "_putchar\n", 9);
 }
 
-int main(void)//Main - Entry point//
+int main(void)/*Main - Entry point*/
 {
 print_putchar();
 return (0);
