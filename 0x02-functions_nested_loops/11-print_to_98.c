6f9929ee60a5d6dd90655c9c1180e8e0ae2dc1bd
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 14:25:18 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,54 +1,51 @@
-void print_number(int n);
-
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
  * @n: The starting number.
  */
 void print_to_98(int n)
 {
-if (n <= 98) {
-for (; n < 98; n++) {
-print_number(n);
-print_char(',');
-print_char(' ');
+int is_negative = 0;
+
+if (n > 98) {
+is_negative = 1;
 }
+
+while (n != 98) {
+if (is_negative) {
+n--;
 }
 else
 {
-for (; n > 98; n--) {
-print_number(n);
-print_char(',');
-print_char(' ');
-}
-}
-print_number(98);
-print_char('\n');
+n++;
 }
 
-/* Custom print functions */
-
-/* Print a character to standard output */
-void print_char(char c);
-
-/* Print an integer to standard output */
-void print_number(int n)
-{
-int divisor = 1;
-
-if (n < 0) {
+int temp = n;
+if (temp < 0) {
+temp = -temp;
 print_char('-');
-n = -n;
 }
 
-while (n / divisor > 9) {
+int divisor = 1;
+while (temp / divisor > 9)
+{
 divisor *= 10;
 }
 
 while (divisor != 0) {
-print_char(n / divisor + '0');
-n %= divisor;
+print_char(temp / divisor + '0');
+temp %= divisor;
 divisor /= 10;
 }
+
+if (n != 98) {
+print_char(',');
+print_char(' ');
+}
+}
+
+print_char('9');
+print_char('8');
+print_char('\n');
 }
 
 /* Custom print_char implementation */
