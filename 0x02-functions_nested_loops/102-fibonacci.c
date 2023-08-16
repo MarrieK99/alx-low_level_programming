3c2864ee9570448f4d2218199c4cc5a2924fcf65
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 17:04:24 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-    int i, n;
-    unsigned long int fib1 = 1, fib2 = 2, next;
+int i, n;
+unsigned long int fib1 = 1, fib2 = 2, next;
 
-    n = 50; /* Number of Fibonacci numbers to generate */
+n = 50; /* Number of Fibonacci numbers to generate */
 
-    for (i = 1; i <= n; i++)
-    {
-        if (i == 1)
-            printf("%lu", fib1);
-        else if (i == 2)
-            printf("%lu", fib2);
-        else
-        {
-            next = fib1 + fib2;
-            printf(", %lu", next);
-            fib1 = fib2;
-            fib2 = next;
-        }
-    }
+for (i = 1; i <= n; i++)
+{
+if (i == 1)
+printf("%lu", fib1);
+else if (i == 2)
+printf("%lu", fib2);
+else
+{
+next = fib1 + fib2;
+printf(", %lu", next);
+fib1 = fib2;
+fib2 = next;
+}
+}
 
     printf("\n");
 
