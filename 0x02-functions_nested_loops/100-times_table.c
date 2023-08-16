f4623374ccc2d71c3dbaeeadc65dc89b11b73d5d
MarrieK99 <maryannekrnj@gmail.com>
2023-08-15 17:17:30 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,57 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
-/**
- * print_times_table - Prints the n times table, starting with 0.
- * @n: The value specifying the times table
- */
-void print_times_table(int n)
-{
-    if (n <= 15 && n >= 0)
-    {
-        int i, j, product;
-        char buffer[10];
+void print_times_table(int n) {
+  if (n > 15 || n < 0) {
+    return;
+  }
 
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                product = i * j;
-                if (j == 0)
-                {
-                    int k = 0;
-                    if (product == 0)
-                        _putchar('0');
-                    else
-                    {
-                        while (product > 0)
-                        {
-                            buffer[k++] = (product % 10) + '0';
-                            product /= 10;
-                        }
-                        for (int l = k - 1; l >= 0; l--)
-                            _putchar(buffer[l]);
-                    }
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    int k = 0;
-                    if (product == 0)
-                        _putchar('0');
-                    else
-                    {
-                        while (product > 0)
-                        {
-                            buffer[k++] = (product % 10) + '0';
-                            product /= 10;
-                        }
-                        for (int l = k - 1; l >= 0; l--)
-                            _putchar(buffer[l]);
-                    }
-                }
-            }
-            _putchar('\n');
-        }
+  for (int i = 0; i <= 10; i++) {
+    for (int j = 1; j <= n; j++) {
+      _putchar(i * j + '0');
+      if (j < n) {
+        _putchar(' ');
+      }
     }
+    _putchar('\n');
+  }
 }
