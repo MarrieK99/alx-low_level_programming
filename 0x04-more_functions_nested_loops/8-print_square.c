89e52bee15991832e9aa06148a714d8b06680888
MarrieK99 <maryannekrnj@gmail.com>
2023-08-17 10:53:09 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,20 +9,20 @@
  */
 void print_square(int size)
 {
-    int row, col;
+int row, col;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+for (row = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+}
+_putchar('#');
+}
+_putchar('\n');
+}
 }
