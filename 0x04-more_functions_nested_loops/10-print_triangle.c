81b97f6311c118263d4e0d5bcfbef3f5e58b897a
MarrieK99 <maryannekrnj@gmail.com>
2023-08-17 11:02:03 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,23 +10,23 @@
  */
 void print_triangle(int size)
 {
-    int row, col;
+int row, col;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (row = 1; row <= size; row++)
-    {
-        for (col = 1; col <= size; col++)
-        {
-            if (col <= size - row)
-                _putchar(' ');
-            else
-                _putchar('#');
-        }
+for (row = 1; row <= size; row++)
+{
+for (col = 1; col <= size; col++)
+{
+if (col <= size - row)
+_putchar(' ');
+else
+_putchar('#');
+}
         _putchar('\n');
     }
 }
