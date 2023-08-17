c8ca052db9cd66f8efcc30c9c6704dbdf5d5693c
MarrieK99 <maryannekrnj@gmail.com>
2023-08-17 10:45:19 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,14 @@
  */
 void print_most_numbers(void)
 {
-    char num;
+char num;
 
-    for (num = '0'; num <= '9'; num++)
-    {
-        if (num != '2' && num != '4')
-        {
-            _putchar(num);
-        }
-    }
-    _putchar('\n');
+for (num = '0'; num <= '9'; num++)
+{
+if (num != '2' && num != '4')
+{
+_putchar(num);
+}
+}
+_putchar('\n');
 }
