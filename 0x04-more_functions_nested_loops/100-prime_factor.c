a3befd3c9580892167bc0adb11b4a547931ce06a
MarrieK99 <maryannekrnj@gmail.com>
2023-08-17 11:06:25 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <math.h>
-
-/**
- * largest_prime_factor - Finds and prints the largest prime factor of a number
- * @n: The number to find the largest prime factor of
- *
- * Description: This function calculates the largest prime factor of
- * the given number 'n'. It iterates through possible divisors starting
- * from 2 up to the square root of 'n', dividing 'n' by each divisor
- * as long as it's divisible. The last value of 'n' will be the largest
- * prime factor.
- */
+/** Finds largest prime factor of a number */
 void largest_prime_factor(unsigned long n)
 {
-    unsigned long divisor = 2;
+unsigned long divisor = 2;
 
-    while (n > 1)
-    {
-        if (n % divisor == 0)
-        {
-            n /= divisor;
-        }
-        else
-        {
-            divisor++;
-        }
-    }
+while (n > 1)
+{
+if (n % divisor == 0)
+{
+n /= divisor;
+}
+else
+{
+divisor++;
+}
+}
 
     printf("%lu\n", divisor);
 }
