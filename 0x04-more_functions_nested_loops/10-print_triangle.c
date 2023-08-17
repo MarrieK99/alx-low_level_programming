05bf5d648f6062cd75d31440a00d6f439ea71038
MarrieK99 <maryannekrnj@gmail.com>
2023-08-17 10:26:21 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle followed by a new line
+ * @size: Size of the triangle
+ *
+ * Description: Prints a right-angled triangle made of '#'
+ * characters. The base of the triangle is determined by
+ * the 'size' parameter.
+ */
+void print_triangle(int size)
+{
+    int row, col;
+
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (row = 1; row <= size; row++)
+    {
+        for (col = 1; col <= size; col++)
+        {
+            if (col <= size - row)
+                _putchar(' ');
+            else
+                _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
