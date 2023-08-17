1c81a785e268b75d68a1985c2d49feec4421e881
MarrieK99 <maryannekrnj@gmail.com>
2023-08-17 10:40:08 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
  * @a: The first integer
  * @b: The second integer
  *
- * Return: The result of the multiplication of a and b
+ * Return: The result of the X of a & b
  */
 int mul(int a, int b)
 {
