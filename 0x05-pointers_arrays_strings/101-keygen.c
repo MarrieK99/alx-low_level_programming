5243716b50cf64c07cac4dbef10db4e0a586eae5
MarrieK99 <maryannekrnj@gmail.com>
2023-08-21 18:12:36 +0300

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - The entry point of the program.
  *
- * Return: The exit status of the program.
+ * Return: The exit status of the program (0 on success).
  */
 int main(void)
 {
-char password[7];  /*6 characters for password + 1 for null terminator*/
+int i;  /* Move the loop variable declaration here */
+char password[7];  /* 6 characters for password + 1 for null terminator */
 const char charset[] = "abcdefghiABCDEFGHIJKLMNOPQRSTUVWXY3456789";
 int charset_length = sizeof(charset) - 1;
 
-srand(time(NULL));  /* Initialize random number generator w/current time*/
+srand(time(NULL));  /* Initialize random number generator with current time */
 
-int i;  /* Move the loop variable declaration here*/
-
-for (i = 0; i < 6; i++)  /* Use loop variable outside of the loop declaration*/
+for (i = 0; i < 6; i++)  /* Use loop variable outside loop declaration */
 {
-int index = rand() % charset_length;  /* Generate random index within charset*/
-password[i] = charset[index];  /* Assign random character to password*/
+int index = rand() % charset_length;/* Generate rndm indx inside charset */
+password[i] = charset[index];  /* Assign random character to password */
 }
 
-password[6] = '\0';  /* Null-terminate the password*/
+password[6] = '\0';  /* Null-terminate the password */
 
 printf("Generated password: %s\n", password);
 
