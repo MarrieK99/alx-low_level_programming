71212abfeda6088e7fe3f3dc2551ed25f037b870
MarrieK99 <maryannekrnj@gmail.com>
2023-08-21 18:04:56 +0300

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+ * main - The entry point of the program.
+ *
+ * Return: The exit status of the program.
+ */
 int main(void)
 {
 char password[7];  /*6 characters for password + 1 for null terminator*/
@@ -10,7 +14,9 @@ int charset_length = sizeof(charset) - 1;
 
 srand(time(NULL));  /* Initialize random number generator w/current time*/
 
-for (int i = 0; i < 6; i++)
+int i;  /* Move the loop variable declaration here*/
+
+for (i = 0; i < 6; i++)  /* Use loop variable outside of the loop declaration*/
 {
 int index = rand() % charset_length;  /* Generate random index within charset*/
 password[i] = charset[index];  /* Assign random character to password*/
