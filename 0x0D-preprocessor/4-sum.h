4ff780ec582bd6c7fdc4ac0568eb72da6e08b7a3
MarrieK99 <maryannekrnj@gmail.com>
2023-09-11 19:22:58 +0300

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,12 @@
+/* ABS - Compute the absolute value of a number
+ *
+ * This macro takes a single argument 'x' and computes its absolute value.
+ * If 'x' is less than 0, it negates 'x' to make it positive; otherwise, it keeps 'x' as is.
+ *
+ * @x: The number for which to compute the absolute value.
+ *
+ * Return: The absolute value of 'x'.
+ */
 #ifndef SUM
 #define SUM(x, y) ((x) + (y))
 #endif
