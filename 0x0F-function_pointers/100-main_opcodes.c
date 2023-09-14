ee3b0efda09aef8d1d603416f534b467d5ea675c
MarrieK99 <maryannekrnj@gmail.com>
2023-09-14 21:10:03 +0300

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function_pointers.h"
 
 /**
- * print_opcodes - Prints the opcodes of its own main function
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success, or an exit code on failure
  */
-void print_opcodes(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int num_bytes;
+        if (argc != 2)
+        {
+        printf("Error\n");
+        return (1);
+        }
 
-/* Check if the number of arguments is correct */
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
+        int num_bytes = atoi(argv[1]);
+
+        if (num_bytes < 0)
+        {
+        printf("Error\n");
+        return (2);
+        }
+
+        unsigned char *main_ptr = (unsigned char *)main;
+
+        int i;
+
+        for (i = 0; i < num_bytes; i++)
+        {
+        printf("%02x", main_ptr[i]);
+        if (i < num_bytes - 1)
+                printf(" ");
+        else
+                printf("\n");
+        }
+
+        return (0);
 }
 
-num_bytes = atoi(argv[1]);
-
-/* Check if the number of bytes is negative */
-if (num_bytes < 0)
-{
-printf("Error\n");
-exit(2);
-}
-
-int i;
-for (i = 0; i < num_bytes; i++)
-{
-printf("%02x", print_opcodes)[i]);
-if (i < num_bytes - 1)
-printf(" ");
-}
-printf("\n");
-}
 
